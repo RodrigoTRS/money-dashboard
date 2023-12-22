17c9b825f65f2d5e0be1507a234c655637a403d5
Rodrigo Teixeira Ribeiro da Silva <rodrigoteix1998@gmail.com>
2023-12-21 22:16:08 -0300

    Finishing course app

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -4,7 +4,8 @@ import { useForm } from "react-hook-form";
 import * as z from 'zod';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TransactionsContext } from "../../contexts/TransactionsContexts";
-import { useContext } from "react";
+import { useContextSelector } from "use-context-selector";
+import { memo } from 'react';
 
 const searchFormSchema = z.object({
     query: z.string(),
@@ -12,9 +13,11 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
-export function SearchForm() {
+function SearchFormComponent() {
 
-    const { fetchTransactions } = useContext(TransactionsContext)
+    const fetchTransactions = useContextSelector(TransactionsContext, (context) => {
+        return context.fetchTransactions;
+    })
 
     const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormInputs>({
         resolver: zodResolver(searchFormSchema),
@@ -38,3 +41,5 @@ export function SearchForm() {
         </SearchFormContainer>
     )
 }
+
+export const SearchForm = memo (SearchFormComponent);
